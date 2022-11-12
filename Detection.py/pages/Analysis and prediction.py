ff837a3194d649b6e1e50a00a306bbfb81a7ab62
Akansha <av0753484@gmail.com>
2022-11-12 19:31:32 +0530

    Update Analysis and prediction.py

--- Detection.py/pages/Analysis and prediction.py
+++ Detection.py/pages/Analysis and prediction.py
@@ -33,13 +33,41 @@ st.image('asset/image1.png')
 csv_path = "HAM10000_metadata.csv"
 skin_df = pd.read_csv(csv_path)
    
-from pydrive.auth import GoogleAuth
-gauth = GoogleAuth()
-gauth.LocalWebserverAuth()
-drive = GoogleDrive(gauth)
+import requests
 
-file_obj = drive.CreateFile({'id': '1l2yBu02uz-qi17Tp4XzhtbSWhd51fUXz'})
-file_obj.GetContentFile('Csv&Data') 
+def download_file_from_google_drive(id, destination):
+    URL = "https://drive.google.com//drive//folders//1l2yBu02uz-qi17Tp4XzhtbSWhd51fUXz?usp=share_link"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)    
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+
+if __name__ == "__main__":
+    file_id = '1l2yBu02uz-qi17Tp4XzhtbSWhd51fUXz'
+    destination = 'DESTINATION FILE ON YOUR DISK'
+    download_file_from_google_drive(file_id, destination)
 
 short_to_full_name_dict = {
     "akiec" : "Bowen's disease", # very early form of skin cancer 
