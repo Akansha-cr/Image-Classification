2c0f2eaf9eb0f0579f06fa561fbe4e448801ff6d
Akansha <av0753484@gmail.com>
2022-11-16 12:21:37 +0530

    Update Analysis and prediction.py

--- Detection.py/pages/Analysis and prediction.py
+++ Detection.py/pages/Analysis and prediction.py
@@ -16,6 +16,11 @@ from fastai.vision.data import *
 import torch
 from torchvision import models
 from torchvision import transforms
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+import numpy as np
+
 
 device = torch.device("cuda")
 
@@ -67,6 +72,19 @@ if submit:
 	_, indices = torch.sort(out, descending=True)
 	[(labels[idx], percentage[idx].item()) for idx in indices[0][:4]]
 
+prediction = st.button("submit")
+if prediction:
+	model = ResNet50(weights='imagenet')
+	Image.open(image_file).convert('RGB')
+	img = image.load_img(image_file, target_size=(224, 224))
+	x = image.img_to_array(img)
+	x = np.expand_dims(x, axis=0)
+	x = preprocess_input(x)
+	preds = model.predict(x)
+# decode the results into a list of tuples (class, description, probability)
+# (one such list for each sample in the batch)
+        print('Predicted:', decode_predictions(preds, top=3)[0])
+	
 	
 #Background image
 def add_bg_from_local(image_file):
