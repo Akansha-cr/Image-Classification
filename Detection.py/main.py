fd0b48775335cdb8cd01f800a5a25e59223d33e9
Akansha <av0753484@gmail.com>
2023-08-21 21:39:27 +0530

    Update main.py

--- Detection.py/main.py
+++ Detection.py/main.py
@@ -1,3 +1,62 @@
+import streamlit as st
+
+# Custom CSS styling
+st.markdown(
+    """
+    <style>
+    .title {
+        font-size: 24px;
+        font-weight: bold;
+        margin-bottom: 20px;
+    }
+    .container {
+        width: 50%;
+        margin: auto;
+        padding: 20px;
+        border: 1px solid #ccc;
+        border-radius: 10px;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+        text-align: center;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# UI layout
+st.title("Welcome to Image Classification Site")
+st.markdown('<p class="title">Classify Different Images</p>', unsafe_allow_html=True)
+
+st.markdown('<div class="container">', unsafe_allow_html=True)
+
+st.write(
+    "Image classification is a fundamental task in computer vision. It involves categorizing images into predefined classes or categories. "
+    "Here are some practical applications:"
+)
+
+st.markdown(
+    """
+    <ol>
+        <li>Object Recognition: Recognizing and labeling objects in images.</li>
+        <li>Medical Imaging: Diagnosing medical conditions from X-rays, MRIs, etc.</li>
+        <li>Security and Surveillance: Identifying threats in security camera feeds.</li>
+        <li>Automated Vehicles: Recognizing pedestrians, road signs, etc.</li>
+        <li>Agriculture: Identifying crop diseases, pests, etc.</li>
+        <li>E-commerce: Product categorization, recommendation systems.</li>
+        <li>Social Media: Content moderation, image tagging.</li>
+        <li>Natural Sciences: Classifying species, celestial objects.</li>
+        <li>Quality Control: Inspecting products for consistency.</li>
+        <li>Document Analysis: Reading handwriting, converting scans to text.</li>
+    </ol>
+    """,
+    unsafe_allow_html=True
+)
+
+st.markdown('</div>', unsafe_allow_html=True)
+
+
+####code under development
+'''
 import pyrebase
 import streamlit as st
 from datetime import datetime
@@ -85,9 +144,10 @@ elif choice == 'Login':
 		user = auth.sign_in_with_email_and_password(email,password)
 		st.write(f'<h1 style="color:purple;font-size:30px;">{"Please head over to Analysis and Prediction page for Prediction."}</h1>',  unsafe_allow_html=True)
 		st.write(f'<h1 style="color:white;font-size:15px;">{"This block is not functional yet, please try again later..."}</h1>',  unsafe_allow_html=True)
-		
-	
-#background image
+
+'''
+
+#FOR BACKGROUND IMAGE
 def add_bg_from_local(image_file):
     with open(image_file, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
